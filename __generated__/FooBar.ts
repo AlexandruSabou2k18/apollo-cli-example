f9d89e6b0650adadbbae6c68972e477408520d29
Tom Fenech <tomjwfenech@gmail.com>
2018-07-20 11:57:24 +0200

    Fix code generation
    Use `extend type` in client schema to fix error caused by duplicate
    definition of Query. Add successful build output.

---
+++ __generated__/FooBar.ts
@@ -0,0 +1,24 @@
+
+
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: FooBar
+// ====================================================
+
+export interface FooBar {
+  foo: string | null;
+  bar: string | null;
+}
+
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
